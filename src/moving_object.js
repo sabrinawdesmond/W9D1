5fa29d1c1a93467875effde26e86968b282743cc
Sabrina Desmond <sabrinawdesmond@gmail.com>
2023-03-06 17:51:39 -0500

    circle moves

--- src/moving_object.js
+++ src/moving_object.js
@@ -18,7 +18,9 @@ class MovingObject {
   }
 
   move() {
-    this.pos = this.pos += this.vel
+    let newX = this.pos[0] + this.vel[0]
+    let newY = this.pos[1] + this.vel[1]
+    this.pos = [newX, newY]
   }
 }
 
